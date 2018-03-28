44139ca62ce532bf2f76b09b89fe1eb08c90874b
tikhomirovd <dimasta00@gmail.com>
2018-03-28 21:50:30 +0300

    Now you can choose how to start the game

--- main.py
+++ main.py
@@ -1,4 +1,26 @@
 import create_hero
+from load_file import load_file
 
-heroes = create_hero.main()
 
+def start_game():
+    print("Do you want to start a new game? Or continue the old one?")
+    print("1) New game\n2) Continue\n")
+    check_game = input()
+    if check_game == '1':
+        heroes = create_hero.create()
+        name_file = input("Select a name to save") + '.txt'
+        f = open(name_file, 'w')
+        for i in range(len(heroes)):
+            heroes[i].save_file(f)
+        f.close()
+        return heroes
+    if check_game == '2':
+        heroes = load_file()
+        return heroes
+
+    print("No option with this answer")
+    return start_game()
+
+
+if __name__ == "__main__":
+    heroes = start_game()
