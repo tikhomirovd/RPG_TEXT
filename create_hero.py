55d963f6a928df21a45a44e3ddba7c3d2b652405
tikhomirovd <dimasta00@gmail.com>
2018-03-28 15:42:42 +0300

    Add parser for txt file

--- create_hero.py
+++ create_hero.py
@@ -1,21 +1,50 @@
 from hero import Hero
 from check_number import check_int
 
-text_player = 'Введите количество игроков'
-name_txt_stats = ''
-number_of_players = check_int(text_player)
-heroes = []
-mas = [0, 0, 0, 0, 0, 0]
-races = []
 
-def read_races():
+def read_races(name_txt_stats):
+    races = []
     with open(name_txt_stats, 'r') as f:
         for line in f:
             race_stats = line.split()
+            print(race_stats)
             races.append(race_stats)
+    return  races
+
+def choose_race(heroes, races, name):
+
+    race = input("Choose your races ")
+    flag = False
+
+    for i in range(len(races)):
+        if race == races[i][0]:
+            print(races)
+            heroes.append(Hero(name, *races[i]))
+            flag = True
+
+    if flag:
+        return races
+    else:
+        print("There is no races with this name ")
+        return choose_race(heroes, races, name)
+
+
+def main() -> list:
+
+    name_txt_stats = 'races_stats.txt'
+    text_player = 'Введите количество игроков'
+    text_races = '''Priest
+    Mag'''
+    number_of_players = check_int(text_player)
+    heroes = []
+    races = read_races(name_txt_stats)
+
+    for i in range(number_of_players):
+        print(text_races)
+        name = input("Input your name")
+        races = choose_race(heroes, races, name)
+    return heroes
 
 
 
-for i in range(number_of_players):
 
-    heroes.append(Hero(*mas))
