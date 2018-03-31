ca07e03e4e93783faeae155a674691364233f1c6
tikhomirovd <dimasta00@gmail.com>
2018-03-31 08:09:48 +0300

    All game was looped

--- main.py
+++ main.py
@@ -1,12 +1,7 @@
 import create_hero
 import game
 
-
-
-
-
-
-
 if __name__ == "__main__":
     heroes, name_file = game.start_game()
-    game.menu(heroes, name_file)
+    while True:
+        game.menu(heroes, name_file)
